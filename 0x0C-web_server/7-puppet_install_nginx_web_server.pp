8b6c94b62212262c9412c30edc1dfbbf82783bcc
OG-1534 <gershomochanda@gmail.com>
2024-03-27 14:41:38 -0700

    configuring your server with Puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+# Install and configure an Nginx server using Puppet
+# Nginx should be listening on port 80
+# Querying Nginx at its root / with a GET request, curl,
+# it must return a page that contains the string Hello World!
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
