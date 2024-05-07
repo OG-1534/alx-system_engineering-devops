b819b7116fe572775ac760ed0944116b87eb7b05
OG-1534 <gershomochanda@gmail.com>
2024-05-07 23:51:39 +0300

    function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-Number of subscribers for a given subreddit
-"""
+
+"""Function that queries the Reddit API"""
 
 from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    Returns the number of subscribers (not active users,
+    total subscribers) for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):
