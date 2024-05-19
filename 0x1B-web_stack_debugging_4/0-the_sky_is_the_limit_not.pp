93fb9f8e2380bfb53ff13512ff072ebd664404a6
OG-1534 <gershomochanda@gmail.com>
2024-05-18 17:20:34 -0700

    let’s fix our stack so that we get to 0 failed requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,17 @@
-#Increase the amonut of traffic handleed by Nginx server
+#Increase the amount of by Nginx server traffic
 
 #Increse default files ULIMIT
 exec { 'fix--for-nginx':
-	# Modify the vlue of ULIMIT
-	command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-	# Specify the sed command path
-	path => 'usr/local/bin/:/bin/',
+# Modify the vlue of ULIMIT
+command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+# Specify the sed command path
+path    => 'usr/local/bin/:/bin/',
 }
 
 # Restart Nginx
 exec { 'nginx-restart':
-	# Restart Nginx service
-	command => '/etc/init.d/nginx restart',
-	# Specify the init.d script path
-	path => '/etc/init,d/',
+# Restart Nginx service
+command => '/etc/init.d/nginx restart',
+# Specify the init.d script path
+path    => '/etc/init,d/',
 }
